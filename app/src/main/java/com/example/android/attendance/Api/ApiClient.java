2c9a64d8451e4835301ff310e114d6db115336f6
AZAN AHMED SIDDIQUE <azaann3@gmail.com>
2023-12-09 19:29:20 +0545

    Update ApiClient.java

--- app/src/main/java/com/example/android/attendance/Api/ApiClient.java
+++ app/src/main/java/com/example/android/attendance/Api/ApiClient.java
@@ -16,7 +16,7 @@ public class ApiClient {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://192.168.1.71:8000/")
+                .baseUrl("http://192.168.1.01:8000/")
                 .client(okHttpClient)
                 .build();
 
